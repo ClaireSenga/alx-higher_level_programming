e1969feeecc8eeb2662b5a40a448907beabcf4da
ClaireSenga <marieclairemangue2908@gmail.com>
2024-01-06 08:36:07 -0800

    0x00

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word[0:3]
+word[7:]
+word[1:8]
+print(f"First 3 letters: {word[0:3]}")
+print(f"Last 2 letters: {word[7:]}")
+print(f"Middle word: {word[1:8]}")
