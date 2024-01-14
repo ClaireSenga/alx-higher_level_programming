b128c08bbb22d36f253d0a66bdfc5aaa017ed73a
ClaireSenga <marieclairemangue2908@gmail.com>
2024-01-14 11:27:47 -0800

    0x0C

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,3 +26,22 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, id=None, size=None, x=None, y=None):
+    '''method that updates instance attributes via */**args.'''
+    if id is not None:
+        self.id = id
+    if size is not None:
+        self.size = size
+    if x is not None:
+        self.x = x
+    if y is not None:
+        self.y = y
+
+    def update(self, *args, **kwargs)
+    '''updated example attributes via no-keyword & keyword args.'''
+    #print(args, kwargs)
+    if args:
+        self.__update(*args)
+    elif kwargs:
+        self.__update(**kwargs)
