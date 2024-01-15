531dc0becc6a36f9b2012e3d4d332babdf8a0919
ClaireSenga <marieclairemangue2908@gmail.com>
2024-01-15 07:57:03 -0800

    0x0C

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,3 +34,34 @@ class Base:
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_objs))
 
+    @staticmethod
+    def from_json_string(json_string):
+        '''method that returns the list of JSON string representation'''
+        if json_string is None or not json_string:
+            return []
+            return json.dumps(json_string)
+    
+    @classmethod
+    def create(cls, **dictionary):
+        '''method that returns an example with all attributes'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            new = Rectangle(1, 1)
+        elif cls is Square:
+            new = Square(1)
+        else:
+            new = None
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        '''method that returns instances'''
+        from os import path
+        file = "{}.json".format(cls.__name__)
+        if not path.isfile(file):
+            return []
+        with open(file, "r" encoding="utf-8") as f:
+            return [cls.create{**d} for d in cls.from_json_string(f.read())]
+        
