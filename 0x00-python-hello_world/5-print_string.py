a771dd133a4dcd6bbc5eaf539f26beaabe3e44a1
ClaireSenga <marieclairemangue2908@gmail.com>
2024-03-11 05:47:28 -0700

    hellow world

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print("%s" % str * 3) #prints str 3x
-alt_str = "Holberton"
-print("%s" % alt_str) #prints alt_str
+print(f"{str}{str}{str}")
+print(f"{str[:9]}")
