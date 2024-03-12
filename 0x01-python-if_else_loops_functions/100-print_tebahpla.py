bbaec9f8ef3b26cba31e4d8505ee052f6ae9f0f2
ClaireSenga <marieclairemangue2908@gmail.com>
2024-03-12 11:33:44 -0700

    loops

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(122, 96, -1):
-    print("{}".format(chr(i).lower() if (122 - i) % 2 == 0 else chr(i).upper(), end=''))
+    char = chr(i).lower() if (122 - i) % 2 == 0 else chr(i).upper()
+    print("{}".format(char), end='')
