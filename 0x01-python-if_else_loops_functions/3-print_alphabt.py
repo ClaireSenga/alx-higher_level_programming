7313b6aa0beb6022faca1cbcff9c4023bb2b7cd3
ClaireSenga <marieclairemangue2908@gmail.com>
2024-03-12 10:41:30 -0700

    loops

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 for char in range(97, 123): #97 to 123 is lowercase
     if chr(char) not in ['e' , 'q']:
-        print("{}".format(chr(char)), end='')
+        print("{}".format(chr(char)) , end='')
