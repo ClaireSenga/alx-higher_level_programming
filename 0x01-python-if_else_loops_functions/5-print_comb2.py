d3633dee948714eb389b0b5a8231539a418ca369
ClaireSenga <marieclairemangue2908@gmail.com>
2024-03-12 11:03:39 -0700

    loops

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(1, 100):
     if i == 99:
-        print("{:02d} ".format(i), end='')
+        print("{:02d}".format(i), end='')
     else:
         print("{:02d}, ".format(i), end='')
