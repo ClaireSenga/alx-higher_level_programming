e400792fc6361306144b70f51f51803b6bd36b20
ClaireSenga <marieclairemangue2908@gmail.com>
2024-03-13 15:45:27 -0700

    loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):   # to avoid repetition
-        print("{:d}{:d},".format(i, j), end=' ' if i != 8 or j != 9 else '')
 
-# Print the combination 89 separately
-print(" 89")
+for i in range(10):
+    for j in range(i + 1, 10):  # to avoid repetition
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{},".format(i, j), end=' ')
