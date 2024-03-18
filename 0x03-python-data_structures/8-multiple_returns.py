9e93501397f98add96baffd621a0fb7cd183e396
ClaireSenga <marieclairemangue2908@gmail.com>
2024-03-18 03:31:31 -0700

    lists

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    # a tuple with len of string & 1st char
 
     if len(sentence) == 0:
         return (None, None)  # 2 because it is a tuple
