1a62789900d568ae374a805609fa55978f72020a
ClaireSenga <marieclairemangue2908@gmail.com>
2024-03-20 14:38:45 -0700

    keys update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-if key in a_dictionary:
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
         a_dictionary[key] = value
     else:  # key does not exist
         a_dictionary[key] = value
