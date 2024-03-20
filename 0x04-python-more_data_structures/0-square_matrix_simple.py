d6c6e1b1e9c2074049bbe2255e71b16e0d439e85
ClaireSenga <marieclairemangue2908@gmail.com>
2024-03-20 10:51:24 -0700

    squared matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+
+    new_matrix = []  # create new matrix
+
+    for row in matrix:
+        new_row = list(map(lambda x: x ** 2, row))
+        new_matrix.append(new_row)
+
+    return new_matrix
