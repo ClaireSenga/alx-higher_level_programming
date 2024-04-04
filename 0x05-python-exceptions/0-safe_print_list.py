e1a5ea8297b6d9c9dc2abdb76a1ccaf718c1fa0d
ClaireSenga <marieclairemangue2908@gmail.com>
2024-04-04 14:15:18 -0700

    print list of x elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,12 @@
-
 #!/usr/bin/python3
+"""a function that priints x elements of a list"""
+
+
 def safe_print_list(my_list=[], x=0):
-    num = 0
+    """create function and set x to default zero"""
+
+    num = 0  # initialise a variable to keep track of no. of elements
+
     for i in range(x):
         try:
             print(my_list[i], end="")
