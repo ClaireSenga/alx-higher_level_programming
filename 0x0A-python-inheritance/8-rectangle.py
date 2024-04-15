1664f7a8158f8e6dd471ff7bc04e65c28671f7b0
ClaireSenga <marieclairemangue2908@gmail.com>
2024-04-15 13:14:55 -0700

    inheritance using super

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,16 +4,15 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represent a rectangle using BaseGeometry."""
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle.
-
+        """instantiation of new Rectangle
         Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-        """
-        self.integer_validator("width", width)
+            width (int): Width of Rectangle
+            height (int): Height of Rectangle
+            """
+
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
