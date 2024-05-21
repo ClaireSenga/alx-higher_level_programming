7c905ba0a812d043cf51eb176b91db6d3bca59d9
ClaireSenga <marieclairemangue2908@gmail.com>
2024-05-21 16:23:07 -0700

    create a table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,3 @@
--- a script that creates a table
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256);
+-- a script that creates a table called 'first table'
+-- 'first_tble should have (id INT) & (name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
