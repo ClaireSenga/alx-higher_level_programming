5d12661b2308bfb86a11db33138996fc2bdfa571
ClaireSenga <marieclairemangue2908@gmail.com>
2024-05-21 17:14:41 -0700

    add mul rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 -- scripts should create 4 records
 
-INSERT INFO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INFO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
-INSERT INFO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INFO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
