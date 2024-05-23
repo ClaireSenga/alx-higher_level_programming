2f1bf88306b6bb466d64dc1f753cef6797868ce4
ClaireSenga <marieclairemangue2908@gmail.com>
2024-05-22 17:34:19 -0700

    import the database

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- imports the database
+
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
