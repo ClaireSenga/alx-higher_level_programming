2df4aefe9066db869e4bf53833ba6dc03f64bc4e
ClaireSenga <marieclairemangue2908@gmail.com>
2024-05-22 17:32:41 -0700

    database

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- a script that lists all the shows contained 
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
