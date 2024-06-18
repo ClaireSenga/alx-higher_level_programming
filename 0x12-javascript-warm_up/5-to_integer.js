97d54d58ce6fbefc19bb87ce781a82dd8937c63c
ClaireSenga <marieclairemangue2908@gmail.com>
2024-06-18 13:59:58 -0700

    if 1st arg can be converted to int

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that prints arg converted to int
+const arg = process.argv[2];
+intValue = parseInt(arg);
+if (isNaN(process.argv[2])) {
+	console.log('Not a number');
+}
+else {
+	console.log("My number: " + intValue);
+}
