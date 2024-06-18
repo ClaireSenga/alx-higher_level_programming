d2eb79464a9aaf6213c5f82c6ebb8da884b7286b
ClaireSenga <marieclairemangue2908@gmail.com>
2024-06-18 14:46:18 -0700

    prints a loop

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//prints x times a string
+const x = process.argv[2];
+const arg = parseInt(x);
+if (isNaN(arg)) {
+	console.log("Missing number of occurrences");
+	}
+else {
+	for (let i = 0; i < arg; i++) {
+	console.log("C is fun");
+}
+}
