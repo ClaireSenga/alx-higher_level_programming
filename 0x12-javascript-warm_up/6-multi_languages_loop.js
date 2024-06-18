9d3edfd05ebbc52aa9026f2850081d78bc7ac372
ClaireSenga <marieclairemangue2908@gmail.com>
2024-06-18 14:21:56 -0700

    string array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 //prints 3 lines using an array
 const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (let i = 0; i < languages.length; i++) {
-	console.log(languages[i]);}
+for (let i = 0; i < languages.length; i++)
+{
+	console.log(languages[i]);
+}
