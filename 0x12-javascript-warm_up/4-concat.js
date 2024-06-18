56f0c4f2be9b1dc6603d692ffb015cf885c4a344
ClaireSenga <marieclairemangue2908@gmail.com>
2024-06-18 12:42:48 -0700

    2 args passed

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// a script that prints 2 args passed
+
+if (process.argv[2] && process.argv[3]) {
+    console.log(`${process.argv[2]} is ${process.argv[3]}`);
+} else {
+    console.log("Missing arguments");
+}
