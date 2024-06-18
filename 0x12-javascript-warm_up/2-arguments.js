356cfe666752079b2ab5f04e2ab9ee2705aa037a
ClaireSenga <marieclairemangue2908@gmail.com>
2024-06-18 12:08:47 -0700

    no of args via process.argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that checks no. of args via process.argv
+
+if (process.argv.length == 2) {
+	console.log('No argument');
+	}
+else if (process.argv.length == 3) {
+	console.log('Argument found');
+	}
+else {
+	console.log('Arguments found');
+	}
