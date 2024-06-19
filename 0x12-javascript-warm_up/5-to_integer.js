e022eafd1578007d2ecb9a4570a261659d41fb1c
ClaireSenga <marieclairemangue2908@gmail.com>
2024-06-19 05:21:46 -0700

    1st arg converted to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 // Script that prints arg converted to int
+
 const arg = process.argv[2];
 const intValue = parseInt(arg);
 
-if (isNaN(process.argv[2])) {
+if (isNaN(intValue)) {
   console.log('Not a number');
 } else {
-  console.log("My number: " + intValue);
+  console.log('My number: ' + intValue);
 }
