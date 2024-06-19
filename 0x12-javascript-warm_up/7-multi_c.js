c386f59cc37d698fd58d7c4018b16252d980209e
ClaireSenga <marieclairemangue2908@gmail.com>
2024-06-19 05:38:15 -0700

    prints x amounts of times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,8 +2,9 @@
 // Prints x times a string
 const x = process.argv[2];
 const arg = parseInt(x);
+
 if (isNaN(arg)) {
-  console.log('Missing number of occurrences');	
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < arg; i++) {
     console.log('C is fun');
