2632bdae8c6095a1fa2f00f0fa510a80afb72b20
ClaireSenga <marieclairemangue2908@gmail.com>
2024-06-19 06:11:37 -0700

    adition of 2 ints

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// A script that prints addition of 2 ints
+function add(a, b) {
+  return a + b;
+}
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+const int1 = parseInt(arg1);
+const int2 = parseInt(arg2);
+
+if isNaN(int1) || isNaN(int2)) {
+  console.log('NaN');
+} else {
+  console.log(add(int1, int2))
+}
