4259f7fec665909426b957e7fdc0313d0b7619d8
ClaireSenga <marieclairemangue2908@gmail.com>
2024-06-19 01:32:54 -0700

    prints first arg passed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-//a script that prints 1st arg passed(psn[2])
+// a script that prints 1st arg passed(psn[2])
 if (process.argv[2]) {
-	console.log(process.argv[2]);
-}
-else {
-	console.log('No argument');
+		console.log(process.argv[2]);
+} else {
+		console.log('No argument');
 }
