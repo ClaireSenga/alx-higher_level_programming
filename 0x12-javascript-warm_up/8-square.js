79e8c714bc7f13f24a5e0fc4af3f4334c8bbdbb8
ClaireSenga <marieclairemangue2908@gmail.com>
2024-06-19 05:54:40 -0700

    prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
-//prints a square
+// Prints a square
 const square = process.argv[2];
 const arg = parseInt(square);
+
 if (isNaN(arg)) {
-	console.log("Missing Size");
-}
-else {
-	for (let i = 0; i < arg;
-		i++) {
-		let row = "";
-	for (let j = 0; j < arg;
-		j++) {
-		row += "X";
-	}
-	console.log(row);
-	}
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < arg; i++) {
+    let row = '';
+    for (let j = 0; j < arg; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
 }
