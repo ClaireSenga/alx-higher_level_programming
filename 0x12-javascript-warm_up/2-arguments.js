1fb8e33061062b00c9ff55b755f041588573fc6e
ClaireSenga <marieclairemangue2908@gmail.com>
2024-06-19 01:27:57 -0700

    prints depending of no. of args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // a script that checks no. of args via process.argv
 
-if (process.argv.length == 2) {
+if (process.argv.length === 2) {
 	console.log('No argument');
 	}
-else if (process.argv.length == 3) {
+else if (process.argv.length === 3) {
 	console.log('Argument found');
 	}
 else {
