940c86df32547d6702859deda005648204689eb2
ClaireSenga <marieclairemangue2908@gmail.com>
2024-07-09 14:53:40 -0700

    advanced

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,33 +1,30 @@
-#!/usr/bin/env python3
-# Fetches 10 most recent commits from a GitHub repository using GitHub API with requests & sys
+#!/usr/bin/python3
+"""
+List 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails”
+Use the GitHub API, here is the documentation
+https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+"""
 
-import requests
-import sys
 
-if len(sys.argv) != 3:
-    print("Usage: python script.py <repository_name> <owner_name>")
-    sys.exit(1)
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
 
-repository_name = sys.argv[1]
-owner_name = sys.argv[2]
-base_url = f'https://api.github.com/repos/{owner_name}/{repository_name}/commits'
-params = {'per_page': 10}  # Limit to 10 most recent commits messages
+    repo = argv[1]
+    owner = argv[2]
+    i = 0
 
-try:
-    # Send GET request to GitHub API.
-    response = requests.get(base_url, params=params, auth=('YOUR_USERNAME', 'YOUR_TOKEN'))
-    
-    # Check if request was a success.
-    if response.status_code == 200:
-        commits = response.json()
-        
-        for commit in commits:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
-    
-    else:
-        print(f"Error: {response.status_code} - {response.text}")
+    URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
 
-except requests.RequestException as e:
-    print(f"Error fetching {base_url}: {e}")
+    response = get(URL)
+    json = response.json()
+
+    for element in json:
+        if i > 9:
+            break
+        sha = element.get('sha')
+        author = element.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author))
+        i += 1
