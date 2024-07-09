9d0c6dfd80a4a287bf7592dc3e85e0265231deaf
ClaireSenga <marieclairemangue2908@gmail.com>
2024-07-09 14:56:23 -0700

    networks

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python3
-# Fetches GitHub user id using GitHub API with Basic Authentication using requests & sys
+#!/usr/bin/python3
+"""
+Takes your GitHub credentials (username & password) & uses the GitHub API
+to display your id
+"""
 
-import requests
-import sys
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
 
-if len(sys.argv) != 3:
-    print("Usage: python script.py <username> <personal_access_token>")
-    sys.exit(1)
+    username = argv[1]
+    password = argv[2]
 
-username = sys.argv[1]
-token = sys.argv[2]
+    URL = "https://api.github.com/user"
+    response = get(URL, auth=(username, password))
+    json = response.json()
 
-url = f'https://api.github.com/user'
-
-try:
-    # Send GET request to GitHub API with Basic Authentication using token
-    response = requests.get(url, auth=(username, token))
-    
-    # Check if request was successful
-    if response.status_code == 200:
-        user_info = response.json()
-        print(f"User ID: {user_info['id']}")
-    else:
-        print(f"Error: {response.status_code} - {response.text}")
-
-except requests.RequestException as e:
-    print(f"Error fetching {url}: {e}")
-
+    print(json.get('id'))
