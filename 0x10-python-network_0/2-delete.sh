58acb64a854a1435deb8894b4032a05f20b724fe
ClaireSenga <marieclairemangue2908@gmail.com>
2024-07-09 13:16:01 -0700

    deletes

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A script that sends a DELETE request to the URL passed
-# as the first argument & displays the body of the response.
+# A script that sends a DELETE request to the URL passed as the first argument & displays the body of the response.
 curl -sX DELETE "$1"
