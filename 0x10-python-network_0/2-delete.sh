981f2d91fd9eb10651e4ecf7c2c217449281e4fe
ClaireSenga <marieclairemangue2908@gmail.com>
2024-07-09 13:09:46 -0700

    network

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A script that sends a DELETE request to the URL passed
-# as the first argument & displays the body of the response
+# as the first argument & displays the body of the response.
 curl -sX DELETE "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A script that takes in a URL, sends a POST request to
-# The passed URL, & displays the body of the response
+# The passed URL, & displays the body of the response.
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
