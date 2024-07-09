d19315c7d09624570f597e230c936ee3854e0137
ClaireSenga <marieclairemangue2908@gmail.com>
2024-07-09 13:18:06 -0700

    methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A script that takes in a URL & displays all HTTP methods
-# the server will accept.
+# A script that takes in a URL & displays all HTTP methods the server will accept.
 curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
