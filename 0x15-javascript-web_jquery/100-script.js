b4351ca742a748efd2eb1d8097c586a0fd52b90d
ClaireSenga <marieclairemangue2908@gmail.com>
2024-08-07 14:25:29 -0700

    Javascript - web jQuery

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+// DOMContentLoaded event to ensure the script runs
+// after the document is fully loaded
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the header element and change its text color to red
+    document.querySelector("header").style.color = "#FF0000";
+});
